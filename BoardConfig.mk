553052a419b88d69dfb2b2b0895e87cfca3c9669
dragunkorr <dragunkorr@gmail.com>
2013-02-16 04:37:11 -0500

    Build DragunKorr

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -27,9 +27,6 @@
 # inherit from common msm8660
 -include device/htc/msm8660-common/BoardConfigCommon.mk
 
-# Broadcom specific config
--include device/htc/msm8660-common/bcm.mk
-
 # Audio
 BOARD_HAVE_HTC_AUDIO := true
 
@@ -44,19 +41,22 @@ BOARD_KERNEL_CMDLINE := console=ttyHSL0 androidboot.hardware=pyramid no_console_
 # Kernel [Build]
 #TARGET_KERNEL_CONFIG := pyramid_defconfig
 #TARGET_KERNEL_SOURCE := kernel/htc/msm8660
+#TARGET_KERNEL_CUSTOM_TOOLCHAIN := linaro
 #BUILD_KERNEL := true
 
 # Kernel [Prebuilt]
 TARGET_PREBUILT_KERNEL := device/htc/pyramid/prebuilt/kernel
 
-
+# Wifi
+WIFI_DRIVER_MODULE_NAME          := bcmdhd
+WIFI_DRIVER_MODULE_PATH          := "/system/lib/modules/bcmdhd.ko"
 
 # Qcom GPS
 BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := pyramid
 
 # RIL
 BOARD_USE_NEW_LIBRIL_HTC := true
-
+#TARGET_PROVIDES_LIBRIL := vendor/htc/pyramid/proprietary/libril.so
 
 # Filesystem
 TARGET_USERIMAGES_USE_EXT4 := true
